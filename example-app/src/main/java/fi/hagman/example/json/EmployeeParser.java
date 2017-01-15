a424cafada37253dc963f11e8b62f15f208dc1dc
ext-hagmaan <anssi.hagman@soikea.com>
2017-01-15 19:29:56 +0200

    Added contractDate

--- example-app/src/main/java/fi/hagman/example/json/EmployeeParser.java
+++ example-app/src/main/java/fi/hagman/example/json/EmployeeParser.java
@@ -1,15 +1,27 @@
 package fi.hagman.example.json;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import org.json.JSONObject;
 
 import fi.hagman.example.model.Employee;
 
 public class EmployeeParser {
 	
+	private static final String DATE_FORMAT = "dd.MM.yyyy";
+	
 	public static JSONObject getEmploeeAsJson(Employee employee){
 		JSONObject employeeJson = new JSONObject();
 		employeeJson.put("firstname", employee.getFirstname());
 		employeeJson.put("lastname", employee.getLastname());
+		
+		if (employee.getContractDate()!=null){
+			DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+			employeeJson.put("contractDate", df.format(employee.getContractDate()));
+		}
+		
+		
 		return employeeJson;
 	}
 	
